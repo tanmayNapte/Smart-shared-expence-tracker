9782015db3630c02ea706f2b4f7e8acef24c36b1
tanmayNapte <tanmaynapte232@gmail.com>
2026-01-08 14:14:05 +0530

    Fixed the issues

--- app.py
+++ app.py
@@ -898,7 +898,6 @@ def logout():
 with app.app_context():
     db.create_all()
     
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port, debug=False)
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
